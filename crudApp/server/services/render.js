bd59760c1f4140970c4ad13da7f880250e92434d
salma sbaiti <salmasbaiti1@gmail.com>
2022-11-28 13:08:16 +0100

    show users

--- crudApp/server/services/render.js
+++ crudApp/server/services/render.js
@@ -1,12 +1,23 @@
+const axios = require('axios')
+
 
 exports.homeRoutes =  (req ,res)=>{
-    res.render("index")
+    //make get request to the api/users
+    axios.get("http://localhost:3000/api/users")
+    .then(function(response){
+        res.render("index" , {users : response.data} )
+    })
+    .catch(err=>{
+        res.send(err)
+    })
 }
 
 exports.add_user =  (req, res)=>{
+    //make post request to the api/users
     res.render("add-user")
 }
 
 exports.update = (req, res)=>{
+    //make put request to api/users
     res.render("update")
 }
