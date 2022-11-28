af0af8a44fab1e483b244c1a152bdc16a1f81aba
salma sbaiti <salmasbaiti1@gmail.com>
2022-11-28 09:49:26 +0100

    mongodb added

--- crudApp/server.js
+++ crudApp/server.js
@@ -4,13 +4,16 @@ const morgan = require('morgan')
 const bodyparser = require("body-parser")
 const path = require('path')
 const app = express();
-
+const connectDB = require('./server/database/connection')
 dotenv.config({path : "config.env"})
 
 const PORT = process.env.PORT || 8080
 
 //log requests
 app.use(morgan('tiny'))
+
+//mongodb connection
+connectDB();
 
 //parse request to body parser
 app.use(bodyparser.urlencoded({extended : true}))
--- crudApp/server/database/connection.js
+++ crudApp/server/database/connection.js
@@ -1,1 +1,15 @@
 const mongoose = require('mongoose')
+
+const connectDB = async ()=>{
+    try{
+        //mongodb connection string
+        const con = await mongoose.connect(process.env.MONGO_URL 
+         )
+        console.log(`mongodb connection ${con.connection.host}`);
+    } catch(err){
+        console.log(err)
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB
