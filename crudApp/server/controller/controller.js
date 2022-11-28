1b2b5d176675c9c994562286c9b7142d004755cc
salma sbaiti <salmasbaiti1@gmail.com>
2022-11-28 11:51:14 +0100

    create done

--- crudApp/server/controller/controller.js
+++ crudApp/server/controller/controller.js
@@ -2,6 +2,32 @@ var Userdb = require('../model/model')
 
 //create a new user
 exports.create =  (req,res)=>{
+    //validing request
+    if(!req.body){
+        res.status(400).send({message : "content can not be empty" })
+        return ;
+    }
+
+    //new user
+    const user1 = new Userdb({
+        name : req.body.name , 
+        email : req.body.email , 
+        gender : req.body.gender , 
+        status : req.body.status 
+
+    })
+
+    //save user in the database
+    user1
+    .save(user1)
+    .then(data=>{
+        res.send(data)
+    })
+    .catch(err =>{
+        res.status(500).send({
+            msg1 : err.message || "some error occured while creating a create operation"
+        })
+    })
 
 }
 //retrive & return all / single user(s)
