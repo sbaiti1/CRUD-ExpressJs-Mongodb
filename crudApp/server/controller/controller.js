f8028b0c82c32b4a6a14b6c4237cab427a182869
salma sbaiti <salmasbaiti1@gmail.com>
2022-11-28 10:05:18 +0100

    api added

---
+++ crudApp/server/controller/controller.js
@@ -0,0 +1,19 @@
+var Userdb = require('../model/model')
+
+//create a new user
+exports.create =  (req,res)=>{
+
+}
+//retrive & return all / single user(s)
+exports.find =  (req,res)=>{
+    
+}
+
+//update
+exports.update =  (req,res)=>{
+    
+}
+//delete
+exports.delete =  (req,res)=>{
+    
+}
--- crudApp/server/routes/router.js
+++ crudApp/server/routes/router.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const route = express.Router()
+
 const services = require('../services/render')
+const controller = require('../controller/controller')
 
 /** 
 *@description root Route
@@ -10,7 +12,12 @@ route.get('/' , services.homeRoutes )
 
 route.get('/add-user' , services.add_user)
 
-
 route.get('/update' , services.update )
+
+//API
+route.post("/api/users" , controller.create)
+route.get("/api/users" , controller.find)
+route.put("/api/users/:id" , controller.update)
+route.delete("/api/users/:id" , controller.delete)
 
 module.exports = route
